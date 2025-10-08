ac3adec0265c1d470d71dbfa85ef366ed54bccd5
Luis Fernando Jaramillo Sánchez <112969620+luis-jaramillo-dev@users.noreply.github.com>
2025-10-08 11:02:12 -0500

    Initial config

---
+++ JavaChallengers/src/main/java/dev/luisjaramillo/chapter2/JavaPrimitives.java
@@ -0,0 +1,9 @@
+package dev.luisjaramillo.chapter2;
+
+public class JavaPrimitives {
+
+    byte javaByte = -128;
+    char javaChar = 32768;
+
+
+}
---
+++ JavaChallengers/src/main/java/dev/luisjaramillo/chapter2/OptionalPractice.java
@@ -0,0 +1,19 @@
+package dev.luisjaramillo.chapter2;
+
+import java.util.Optional;
+
+import static java.util.Optional.ofNullable;
+
+public class OptionalPractice {
+
+    static String finalZionValue ="";
+
+    static int matrixCount=0;
+
+    public static void main(String... args){
+        Optional<String> optFromMatrix = Optional.ofNullable(finalZionValue);
+        var agentSmit = "Virus";
+    }
+}
+
+
